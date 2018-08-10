03f206737b058991b1b4ace73a53bf2abe4519e1
Elaine Anderson <eparie@bu.edu>
2018-08-10 15:06:13 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,10 @@
 class ProductsController < ApplicationController
 
   def index
-    session[:cart] = @cart
   end
 
   def add
-    @cart << params[:product]
+    cart << params[:product]
     redirect_to '/products'
   end
 end
