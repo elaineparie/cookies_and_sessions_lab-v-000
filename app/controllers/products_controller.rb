8e1de018391b493ee9a0a4d557f69157cde457f1
Elaine Anderson <eparie@bu.edu>
2018-08-10 13:34:27 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
-      @product = Product.find_by(id: params[:id])
+      
   end
 
   def add
-    @product = Product.find_by(id: params[:id])
+
   end
 end
